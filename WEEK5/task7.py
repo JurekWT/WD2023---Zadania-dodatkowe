c13aa2abdc22037f3138639619aa170b0e1ba0e1
Jerzy Tymofiejczyk <128184858+JurekWT@users.noreply.github.com>
2023-04-01 18:08:57 +0200

    poprawka

--- WEEK5/task7.py
+++ WEEK5/task7.py
@@ -4,5 +4,5 @@ def calculate_tax(income, /, *, tax_rate=0.1):
 
 
 przychod = 2137.69
-podatek = calculate_tax(przychod, tax_rate=0.23)
+podatek = calculate_tax(przychod)
 print(f'Podatek od kwoty {przychod} wynosi {podatek:.2f}')
